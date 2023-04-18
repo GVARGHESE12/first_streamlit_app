93f1d62c6df44775002214f824518c70ea2ae769
GVARGHESE12 <131146678+GVARGHESE12@users.noreply.github.com>
2023-04-18 14:37:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parents ne healthy Diner')
-streamlit.header('breakfast menu')
-streamlit.text('IDLI')
-streamlit.text('DOSA')
-streamlit.text('POHA')
+streamlit.header('🥑🍞Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
