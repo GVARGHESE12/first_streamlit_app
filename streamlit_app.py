4d3e27fa9eaeb122ca18ec47fc1f84206710e0f2
GVARGHESE12 <131146678+GVARGHESE12@users.noreply.github.com>
2023-04-24 12:16:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 streamlit.title('My parents ne healthy Diner')
 streamlit.header('🥑🍞Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
